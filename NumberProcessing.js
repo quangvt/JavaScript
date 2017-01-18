f7e1ad1085bd10e927b5a8d65efd3df650e60ab7
Vu Trong Quang <vutrongquang@gmail.com>
2017-01-18 18:02:48 +0700

    Update NumberProcessing.js

--- NumberProcessing.js
+++ NumberProcessing.js
@@ -7,3 +7,13 @@
 function randomRange(myMin, myMax) {
   return Math.floor(Math.random()*(myMax-myMin+1)) + myMin;
 }
+
+# Function Name: Factorialize
+function factorialize(num) {
+  if (num === 0){
+    return 1;
+  }
+  else {
+    return num*factorialize(num-1);
+  }
+}
