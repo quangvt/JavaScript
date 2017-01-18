2dd84422f9363f9346bc7f4d75c5ef4e9c4de50c
Vu Trong Quang <vutrongquang@gmail.com>
2017-01-18 18:13:45 +0700

    Update NumberProcessing.js

--- NumberProcessing.js
+++ NumberProcessing.js
@@ -1,6 +1,8 @@
 # Function Name: [Your Function Name]
 # Function List
 #   Find Shortest Word
+#   Factorialize
+#   Largest of Four
 
 ###############################################################################
 # Function Name: Generate Random Range
@@ -8,6 +10,7 @@ function randomRange(myMin, myMax) {
   return Math.floor(Math.random()*(myMax-myMin+1)) + myMin;
 }
 
+###############################################################################
 # Function Name: Factorialize
 function factorialize(num) {
   if (num === 0){
@@ -17,3 +20,18 @@ function factorialize(num) {
     return num*factorialize(num-1);
   }
 }
+
+###############################################################################
+# Function Name: Largest of Four
+function largestOfFour(arr) {
+  return arr.map(function(childarr){
+      return childarr.sort(function(a, b){
+        return b - a;
+      });
+  }).map(function(childarr){
+    return childarr[0];
+  });
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], 
+               [1000, 1001, 857, 1]]);
