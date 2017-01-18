b566b81fb4a0b10af269ebe6c80746d85f794488
Vu Trong Quang <vutrongquang@gmail.com>
2017-01-18 17:48:42 +0700

    Add files via upload

---
+++ NumberProcessing.js
@@ -0,0 +1,9 @@
+# Function Name: [Your Function Name]
+# Function List
+#   Find Shortest Word
+
+###############################################################################
+# Function Name: Generate Random Range
+function randomRange(myMin, myMax) {
+  return Math.floor(Math.random()*(myMax-myMin+1)) + myMin;
+}
