da8346239ce76e87d4d7a75df59de2b4c84791f6
Vu Trong Quang <vutrongquang@gmail.com>
2017-04-03 20:19:32 +0700

    Update 19ES6_VarConstLet.js

--- Beau/19ES6_VarConstLet.js
+++ Beau/19ES6_VarConstLet.js
@@ -1,20 +1,17 @@
 // JS Nuggets: Const vs Let vs Var
 
-// const
+// const => can not change const
 const Pi = 3.14;
 Pi = 1; // Error
 
 // let (=> block level variable)
-// Case 1
 console.log(i); // error
-
 for (let i = 0; i < 3; i++) {
-    console.log(i); // 1 1 0 1 2 1
+    console.log(i); // 0 1 2
 }
-
 console.log(i); // error
 
-// var
+// var (func and entire )
 console.log(j); // undefined => not error (hoisting): existed but not has not assigned yet
 for(var j = 0; j < 3; j++) {
     console.log(j); // 0 1 2
